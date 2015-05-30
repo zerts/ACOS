a24efb12abd0a71a4eb8d1a9cc3638b11b16d22f
Andrey <zertsalov96@yandex.ru>
2015-05-30 14:34:20 +0300

    Update top.c

--- top.c
+++ top.c
@@ -46,24 +46,63 @@ void printProc(int i, struct process currProc)
     printf("%.3f ", currProc.timeUsed);
     printf("%lu\n", currProc.usedMemory);
 }
-
 void getInformationAboutProcess(struct dirent * currProc)
 {
     path = malloc((12 + strlen(currProc->d_name)) * sizeof(char));
+    if (path == NULL)
+    {
+        detectError("malloc failed with creating path");
+        return;
+    }
     sprintf(path, "/proc/%s/stat", currProc->d_name);
     FILE * stats = fopen(path, "r");
+    if (stats == NULL)
+    {
+        detectError("fopen failed");
+        return;
+    }
     processes[numberOfProc].name = malloc(30 * sizeof(char));
-    fscanf(stats, "%d %s", &(processes[numberOfProc].number), processes[numberOfProc].name);
+    if (processes[numberOfProc].name == NULL)
+    {
+        detectError("malloc failed with creating name");
+        return;
+    }
+    if (fscanf(stats, "%d %s", &(processes[numberOfProc].number), processes[numberOfProc].name) != 2)
+    {
+        detectError("scanf failed with reading name and pid");
+        return;
+    }
     //printf("%d %s\n", processes[numberOfProc].number, processes[numberOfProc].name);
     char * uselessInfo = malloc(30 * sizeof(char));
+    if (uselessInfo == NULL)
+    {
+        detectError("malloc failed with creating uselessInfo");
+        return;
+    }
     for (int i = 3; i < 14; i++)
-        fscanf(stats, "%s", uselessInfo);
+        if (fscanf(stats, "%s", uselessInfo) != 1)
+        {
+            detectError("scanf failed with reading useless info");
+            return;
+        }
     long unsigned int utime, stime;
-    fscanf(stats, "%lu %lu", &utime, &stime);
+    if (fscanf(stats, "%lu %lu", &utime, &stime) != 2)
+    {
+        detectError("scanf failed with reading time info");
+        return;
+    }
     processes[numberOfProc].timeUsed = (double)((utime + stime) / sysconf(_SC_CLK_TCK));
     for (int i = 16; i < 23; i++)
-        fscanf(stats, "%s", uselessInfo);
-    fscanf(stats, "%lu ", &(processes[numberOfProc].usedMemory));
+        if (fscanf(stats, "%s", uselessInfo) != 1)
+        {
+            detectError("scanf failed with reading useless info");
+            return;
+        }
+    if (fscanf(stats, "%lu ", &(processes[numberOfProc].usedMemory)) != 1)
+    {
+        detectError("scanf failed with reading memory info");
+        return;
+    }
     //printProc(processes[numberOfProc]);
     fclose(stats);
 }
