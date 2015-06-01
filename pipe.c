777c85d010e7a5b2f533d9a0316625e1f9dbd34a
Andrey <zertsalov96@yandex.ru>
2015-06-01 10:18:18 +0300

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -11,11 +11,11 @@
 #include <sys/ipc.h>
 #include <semaphore.h>
 
-#define SEMAPHORE_NAME "sem__"
+#define SEMAPHORE_NAME "sem"
 #define SHARED_MEMORY_NAME "newPartOfSharedMemory"
-#define STRING_TO_WRITE "bbb"
+#define STRING_TO_WRITE "hello world!"
 
-const size_t SHM_SIZE = 8192;
+const size_t SHM_SIZE = 3000;
 const size_t NUMBER_OF_PIPES;
 int fdescr;
 extern int errno;
@@ -23,7 +23,7 @@ int number_of_pipes = 1;
 
 struct shared_memory
 {
-    sem_t * sem;
+    sem_t * semWrite, * semRead;
     int * begin, * end;
     int * fdescr;
     int * number;
@@ -44,7 +44,6 @@ void create_pipe(struct shared_memory ** fd)
     char shm_name[26] = SHARED_MEMORY_NAME;
     sprintf(shm_name + strlen(SHARED_MEMORY_NAME), "%d", number_of_pipes);
     fdescr = shm_open(shm_name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-    //printf("shm_open\n");
     if (fdescr == -1)
     {
         detectError("shn_open failed");
@@ -61,22 +60,32 @@ void create_pipe(struct shared_memory ** fd)
         detectError("mmap faild");
         return;
     }
-    (*fd)->begin = (int *)(addr);
-    (*fd)->end = (int *)(addr + sizeof(int));
-    (*fd)->fdescr = (int *)(addr + 2 * sizeof(int));
-    (*fd)->number = (int *)(addr + 3 * sizeof(int));
-    (*fd)->addr = (void *)(addr + 4 * sizeof(int));
-    (*fd)->buf = (char *)(addr + 10 * sizeof(int));
-    char sem_name[26] = SEMAPHORE_NAME;
-    sprintf(sem_name + strlen(SEMAPHORE_NAME), "%d", number_of_pipes);
-    (*fd)->sem = sem_open(sem_name, O_CREAT);
-    //printf("sem_open\n");
-    if ((*fd)->sem == SEM_FAILED)
+    (*fd)->semWrite = (sem_t *)(addr);
+    (*fd)->semRead = (sem_t *)(addr + sizeof(sem_t));
+    (*fd)->begin = (int *)(addr + 2 * sizeof(sem_t));
+    (*fd)->end = (int *)(addr + sizeof(int) + 2 * sizeof(sem_t));
+    (*fd)->fdescr = (int *)(addr + 2 * sizeof(int) + 2 * sizeof(sem_t));
+    (*fd)->number = (int *)(addr + 3 * sizeof(int) + 2 * sizeof(sem_t));
+    (*fd)->addr = (void *)(addr + 4 * sizeof(int) + 2 * sizeof(sem_t));
+    (*fd)->buf = (char *)(addr + 10 * sizeof(int) + 2 * sizeof(sem_t));
+    //char sem_name[26] = SEMAPHORE_NAME;
+    //sprintf(sem_name + strlen(SEMAPHORE_NAME), "%d", number_of_pipes);
+    printf("1\n");
+    sem_init((*fd)->semWrite, 1, 1);
+    sem_init((*fd)->semRead, 1, 1);
+    printf("2\n");
+    if ((*fd)->semWrite == SEM_FAILED)
     {
         detectError("sem_open failed");
         return;
     }
-    sem_post((*fd)->sem);
+    if ((*fd)->semRead == SEM_FAILED)
+    {
+        detectError("sem_open failed");
+        return;
+    }
+    sem_post((*fd)->semWrite);
+    sem_post((*fd)->semRead);
     *((*fd)->begin) = 0;
     *((*fd)->end) = 0;
     *((*fd)->number) = number_of_pipes;
@@ -91,33 +100,33 @@ void deletePipe(struct shared_memory * fd)
     sprintf(shm_name + strlen(SHARED_MEMORY_NAME), "%d", *(fd->number));
     shm_unlink(shm_name);
     close(*(fd->fdescr));
-    sem_close(fd->sem);
+    sem_destroy(fd->semWrite);
+    sem_destroy(fd->semRead);
 }
 
 
 void writeToPipe(struct shared_memory * fd, char * buf, size_t size_of_buf)
 {
-    //printf("write begin!\n");
-    //printf("wait in write...\n");
-    if (sem_wait(fd->sem) == -1)
+    if (sem_wait(fd->semWrite) == -1)
     {
         detectError("sem_wait in writeToPipe failed");
         return;
     }
-    //printf("go in write!\n");
     for (size_t tab = 0; tab < size_of_buf; tab++)
     {
         if (*(fd->end) > SHM_SIZE - 5)
+        {
             *(fd->end) = 0;
+        }
+        //printf("%d\n", *(fd->end));
         (fd->buf)[*(fd->end)] = buf[tab];
         *(fd->end) += 1;
     }
-    if (sem_post(fd->sem) == -1)
+    if (sem_post(fd->semWrite) == -1)
     {
         detectError("sem_post in writeToPipe failed");
         return;
     }
-    //printf("write done!\n");
 }
 
 size_t sizeOfPipe(struct shared_memory * fd)
@@ -137,26 +146,25 @@ int readFromPipe(struct shared_memory * fd, char * buf, size_t size_of_part_to_r
         detectError("malloc in readFromPipe failed");
         return -1;
     }
-    //printf("wait...\n");
-    if (sem_wait(fd->sem) == -1)
+    //printf("reading is coming...\n");
+    if (sem_wait(fd->semRead) == -1)
     {
         detectError("sem_wait in readFromPipe failed");
         return -1;
     }
-    //printf("go!\n");
     int tab = 0;
     if (size_of_part_to_read == 0)
         return 0;
+    //printf("reading begin...\n");
     while (*(fd->begin) != *(fd->end) && tab < size_of_part_to_read && sizeOfPipe(fd) > 0)
     {
-        //printf("while in read\n");
-        if (*(fd->begin) == SHM_SIZE)
+        if (*(fd->begin) > SHM_SIZE - 5)
             *(fd->begin) = 0;
         buf[tab] = (fd->buf)[*(fd->begin)];
         *(fd->begin) += 1;
         tab++;
     }
-    if (sem_post(fd->sem) == -1)
+    if (sem_post(fd->semRead) == -1)
     {
         detectError("sem_post in readFromPipe failed");
         return -1;
@@ -172,30 +180,32 @@ int main()
     pid_t t = fork();
     if (t == 0)
     {
-        writeToPipe(fd, STRING_TO_WRITE, strlen(STRING_TO_WRITE));
+        for (int i = 0; i < 1000; i++)
+        {
+            //printf("%d\n", i);
+            writeToPipe(fd, STRING_TO_WRITE, strlen(STRING_TO_WRITE));
+        }
+        //printf("writing is over!\n");
     }
     else
     {
-    //sleep(1);
-        int total = strlen(STRING_TO_WRITE), already_done = 0;
+        int total = 1000 * strlen(STRING_TO_WRITE), already_done = 0;
+        printf("%d\n", total);
         char * buf = malloc(total);
         if (buf == NULL)
         {
             detectError("mallok in child process failed");
             return 0;
         }
-        while(1)
+        //printf("wait...\n");
+        while (already_done < total)
         {
-            while (already_done < total)
-            {
-                //printf("while\n");
-                already_done += readFromPipe(fd, buf + already_done, total - already_done);
-            }
-            //printf("%d\n", total);
-            buf[total] = '\0';
-            printf("buf = %s\n", buf);
-            //sleep(1);
+            //printf("wait...\n");
+            already_done += readFromPipe(fd, buf + already_done, total - already_done);
+            printf("already_done = %d\n", already_done);
         }
+        buf[total] = '\0';
+        printf("buf = %s\n", buf);
         deletePipe(fd);
         return 0;
     }
